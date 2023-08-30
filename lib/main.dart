ee3abd8891bd125f01c128caee92b8e5ee158374
vijai-ks <vijai.ksv@gmail.com>
2023-08-30 21:28:38 +0530

    refactor: dice_page: Update containers with expanded widgets
    Signed-off-by: vijai-ks <vijai.ksv@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -18,24 +18,24 @@ class Dicee extends StatelessWidget {
         ),
         body: SafeArea(
           child: Container(
-            child: Row(
-              children: [
-                Container(
-                  height: 200,
-                  child: Image(
-                    image: AssetImage('images/dice1.png'),
+            child: Center(
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Image(
+                      image: AssetImage('images/dice1.png'),
+                    ),
                   ),
-                ),
-                SizedBox(
-                  width: 10,
-                ),
-                Container(
-                  height: 200,
-                  child: Image(
-                    image: AssetImage('images/dice2.png'),
+                  SizedBox(
+                    width: 10,
                   ),
-                ),
-              ],
+                  Expanded(
+                    child: Image(
+                      image: AssetImage('images/dice2.png'),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
