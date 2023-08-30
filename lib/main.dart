d647c6d2f71db2f4c53263d53f2a33fb26f4ea8d
vijai-ks <vijai.ksv@gmail.com>
2023-08-30 21:05:36 +0530

    feat: dice_page: Add sample dies with containers
    Signed-off-by: vijai-ks <vijai.ksv@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,27 @@ class Dicee extends StatelessWidget {
           backgroundColor: Colors.red,
         ),
         body: SafeArea(
-          child: Container(),
+          child: Container(
+            child: Row(
+              children: [
+                Container(
+                  height: 200,
+                  child: Image(
+                    image: AssetImage('images/dice1.png'),
+                  ),
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                Container(
+                  height: 200,
+                  child: Image(
+                    image: AssetImage('images/dice2.png'),
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
